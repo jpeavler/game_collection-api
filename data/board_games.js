32bcbe1423fbfc2320324245bfbce1ce4c39ed6a
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-20 10:38:04 -0600

    add function to read all board games from MongoDB

---
+++ data/board_games.js
@@ -0,0 +1,40 @@
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+//Connection URL and Database Settings
+const url = process.env.ATLAS_CONNECTION;
+const settings = { useUnifiedTopology: true};
+
+//Database and Collection name
+const dbName = 'game_collection';
+const colName = 'board_games';
+
+//Read all BoardGames
+const getBoardGames = () => {
+    const myPromise = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, function(err, client) {
+            if(err){
+                reject(err);
+            } else{
+                console.log("Connected successfully to server to GET Board Games");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                collection.find({}).toArray(function(err, docs) {
+                    if(err){
+                        reject(err);
+                    } else{
+                        console.log("Found the following Board Games");
+                        console.log(docs);
+                        resolve(docs);
+                        client.close();
+                    }
+                });
+            }
+        });
+    });
+    return myPromise;
+}
+
+module.exports = {
+    getBoardGames
+}
