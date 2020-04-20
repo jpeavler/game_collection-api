655a9075908caa082a55c39367f3576c413aa96d
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-20 11:28:10 -0600

    add board_games api route with get

---
+++ routes/api/board_games.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+const {
+    getBoardGames
+} = require('../../data/board_games')
+
+// GET Game Collection listing
+router.get('/', async function(req, res, next) {
+    try{
+        const data = await getBoardGames();
+        res.send(data);
+    }catch(err){
+        console.log(err);
+        res.send(500, 'Internal Server Issue, check logs');
+    }
+});
+
+module.exports = router;
