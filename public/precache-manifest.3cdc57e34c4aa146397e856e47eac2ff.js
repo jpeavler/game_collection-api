22792ac41bd06c127e9a1b5e220bcd0a228ccd43
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-24 13:21:01 -0600

    01-Deployement with React

---
+++ public/precache-manifest.3cdc57e34c4aa146397e856e47eac2ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b1b8f4631f9f43198766b2da3bc009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d89471b5301d690aa14b",
+    "url": "/static/css/main.89d4897c.chunk.css"
+  },
+  {
+    "revision": "780348c9ddb1fbbee6c0",
+    "url": "/static/js/2.92ca457a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.92ca457a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d89471b5301d690aa14b",
+    "url": "/static/js/main.062c05d7.chunk.js"
+  },
+  {
+    "revision": "3fff733ba56e7e48c18b",
+    "url": "/static/js/runtime-main.52573c1e.js"
+  }
+]);
